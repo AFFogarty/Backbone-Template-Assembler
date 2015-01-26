4e955805539092f09c2ebf800d12a72ec43e27ff
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-01-26 18:52:04 -0500

    New: Underscore formatter function

--- built-template.py
+++ built-template.py
@@ -2,4 +2,11 @@ import os
 
 BUILD_DIRECTORY = "build/"
 HEADER_TEMPLATE_DIRECTORY = "templates"
-BACKBONE_TEMPLATE_DIRECTORY = "templates/underscore-templates/"
+BACKBONE_TEMPLATE_DIRECTORY = "templates/underscore-templates/"BACKBONE_TEMPLATE_DIRECTORY = "templates/underscore-templates/"
+
+def format_underscore_template(name, content):
+	"""
+	Format the template as an Underscore.js template.
+	"""
+	return '<script type="text/template" id="{0}">\n{1}\n</script>'.format(name, content)
+
