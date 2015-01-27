c026992b02452fcdab9e60b8a61c38c7a5d16bce
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-01-26 19:06:13 -0500

    Fixed: Better underscore formatting

--- built-template.py
+++ built-template.py
@@ -8,7 +8,7 @@ def format_underscore_template(name, content):
 	"""
 	Format the template as an Underscore.js template.
 	"""
-	return '<script type="text/template" id="{0}">\n{1}\n</script>'.format(name, content)
+	return '\n<script type="text/template" id="{0}">\n{1}\n</script>\n'.format(name, content)
 
 def assemble_templates(backbone_template_formatter):
 	"""
