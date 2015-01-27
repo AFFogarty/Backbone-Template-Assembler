ebd4ad2542c040e9b531c2cca85325b48aaf62e1
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-01-26 19:00:13 -0500

    New: Assembler function

--- built-template.py
+++ built-template.py
@@ -11,6 +11,21 @@ def format_underscore_template(name, content):
 	return '<script type="text/template" id="{0}">\n{1}\n</script>'.format(name, content)
 
 def assemble_templates(backbone_template_formatter):
+	"""
+	Assemble the header, the footer, and all backbone templates into one string.
+	"""
+	# Grab the header content
+	output = open(HEADER_TEMPLATE_DIRECTOR + "header.html").read()
+	# Attach the backbone templates
+	for file in os.listdir(BACKBONE_TEMPLATE_DIRECTORY).sort():
+	    if file.endswith(".html"):
+	    	name = file.rstrip(".html")
+	    	content = open(BACKBONE_TEMPLATE_DIRECTORY + file, "r").read()
+	        # It is a template, so add it
+	        output += backbone_template_formatter(name, content)
+	# Append the footer content
+	output += open(HEADER_TEMPLATE_DIRECTORY + "footer.html", 'r').read()
+	return output
 
 def build_underscore_templates():
 	# Open the file to build
